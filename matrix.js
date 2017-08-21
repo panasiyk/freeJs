91799ab3092c20553af13df67bd7430a6f3247fa
Andry Panasiuk <andryprnasiyk@gmail.com>
2017-08-22 00:40:23 +0300

    повішаний модель на першу кнопку

--- matrix.js
+++ matrix.js
@@ -9,38 +9,129 @@ var numberForIllumination;
 var arrayOfElementForIllumination;
 var arrayOfIdIlluminationOfElements;
 
-function buttonClickHandler(button){
-    button.style.display = "none";
-    readData();
-    createMatrix();
-    createCloneArray();
-    createTable();
-    addEventLisener();
-    fillTableWithData();
-    fillAverageBlockWithData();
-    fillSumBlockWithData();
-    createClassForFindPercent();
-}
+var startModule = (function () {
+    var startModuleObj = {};
 
+    startModuleObj.buttonClickHandler = function (button){
+        button.style.display = "none";
+        readData();
+        createMatrix();
+        startModuleObj.createCloneArray();
+        createTable();
+        startModuleObj.addEventLisener();
+        startModuleObj.fillTableWithData();
+        startModuleObj.fillAverageBlockWithData();
+        startModuleObj.fillSumBlockWithData();
+        startModuleObj.createClassForFindPercent();
+    };
 
+    function readData(){
+        rowsCount = Number(document.getElementById("column").value);
+        columsCount = Number(document.getElementById("row").value);
+        numberForIllumination = Number(document.getElementById("numberForIllumination").value);
+
+    }
+    function createMatrix(){
+        arrayOfObjects=new Array(rowsCount);
+        for(var i=0; i<rowsCount; i++){
+            arrayOfObjects[i] = new Array(columsCount);
+            for(var j=0; j<columsCount; j++){
+                arrayOfObjects[i][j]=createDatObject();
+            }
+        }
+    }
+
+    startModuleObj.createCloneArray=function (){
+        arrayOfObjectsClone=[];
+        for(var i=0; i<rowsCount; i++){
+            for(var j=0; j<columsCount; j++){
+                arrayOfObjectsClone.push(arrayOfObjects[i][j]);
+            }
+        }
+    }
+
+    function createTable(){
+        for( i=0; i<rowsCount; i++){
+            newrow = mytable.insertRow(i);
+            for( j=0; j<columsCount+1; j++){
+                newCell = newrow.insertCell(j);
+            }
+        }
+        newrow = mytable.insertRow(i);
+        for( j=0; j<columsCount; j++){
+            newCell = newrow.insertCell(j);
+        }
+    }
+    startModuleObj.fillTableWithData = function (){
+        for(var i=0;i<rowsCount;i++){
+            for(var j=0;j<columsCount;j++){
+                mytable.rows[i].cells[j].innerHTML=arrayOfObjects[i][j].amount;
+                mytable.rows[i].cells[j].id = arrayOfObjects[i][j].id;
+            }
+        }
+    };
+    startModuleObj.addEventLisener = function (){
+        for(var i=0; i<rowsCount; i++){
+            for(var j=0; j<columsCount; j++){
+                mytable.rows[i].cells[j].onclick = onCellClick;
+                mytable.rows[i].cells[j].onmouseover = onMouseOverCell;
+                mytable.rows[i].cells[j].onmouseout =  onMouseOutCell;
+                mytable.rows[i].cells[columsCount].onmouseover = onMouseOverSumBlock;
+                mytable.rows[i].cells[columsCount].onmouseout =  onMouseOutSumBlock;
+
+            }
+        }
+    };
+
+    startModuleObj.fillAverageBlockWithData = function (){
+        for (var i=0; i<columsCount; i++){
+            var sum=0;
+            for(var j=0; j<rowsCount; j++){
+                sum += arrayOfObjects[j][i].amount;
+            }
+            mytable.rows[rowsCount].cells[i].innerHTML = (parseInt(sum/columsCount*10)/10);
+        }
+
+    };
+
+    startModuleObj.fillSumBlockWithData = function (){
+        for (var i=0; i<rowsCount; i++){
+            var sum=0;
+            for(var j=0; j<columsCount; j++){
+                sum += arrayOfObjects[i][j].amount;
+            }
+            mytable.rows[i].cells[columsCount].innerHTML = sum;
+        }
+    };
+
+    startModuleObj.createClassForFindPercent = function () {
+        for (var i=0; i<rowsCount; i++){
+            for(var j=0; j<=columsCount; j++){
+                mytable.rows[i].cells[j].className=i;
+            }
+        }
+    }
+
+    return startModuleObj;
+}());
 
 function buttonClickHandlerForAddNewRow() {
     rowsCount++;
     addNewRowInArrayOfObjects();
     addNewRowInCloneArray();
     createTableOfnewRow();
-    addEventLisener();
-    fillTableWithData();
-    fillAverageBlockWithData();
-    fillSumBlockWithData();
-    createClassForFindPercent();
+    startModule.addEventLisener();
+    startModule.fillTableWithData();
+    startModule.fillAverageBlockWithData();
+    startModule.fillSumBlockWithData();
+    startModule.createClassForFindPercent();
 }
 
 function addNewRowInArrayOfObjects() {
-        arrayOfObjects[rowsCount-1]=[];
-        for(var j=0; j<columsCount; j++){
-            arrayOfObjects[rowsCount-1][j]=createDatObject();
-        }
+    arrayOfObjects[rowsCount-1]=[];
+    for(var j=0; j<columsCount; j++){
+        arrayOfObjects[rowsCount-1][j]=createDatObject();
+    }
 }
 
 function createTableOfnewRow() {
@@ -51,99 +142,17 @@ function createTableOfnewRow() {
 }
 
 function addNewRowInCloneArray(){
-        for(var j=0; j<columsCount; j++){
-            arrayOfObjectsClone.push(arrayOfObjects[rowsCount-1][j]);
-        }
-}
-
-function readData(){
-   rowsCount = Number(document.getElementById("column").value);
-   columsCount = Number(document.getElementById("row").value);
-   numberForIllumination = Number(document.getElementById("numberForIllumination").value);
-
-}
-function createMatrix(){
-    arrayOfObjects=new Array(rowsCount);
-    for(var i=0; i<rowsCount; i++){
-      arrayOfObjects[i] = new Array(columsCount);
-        for(var j=0; j<columsCount; j++){
-             arrayOfObjects[i][j]=createDatObject();
-        }
-    }
-}
-
-function createCloneArray(){
-    arrayOfObjectsClone=[];
-    for(var i=0; i<rowsCount; i++){
-        for(var j=0; j<columsCount; j++){
-             arrayOfObjectsClone.push(arrayOfObjects[i][j]);
-        }
-    }
-}
-
-function createDatObject(){
-    var obj = new Object();
-    obj.amount = Math.floor(Math.random()*(1000-100))+100;
-    obj.id=Math.floor(Math.random()*100000);
-    return obj;
-}
-
-function createTable(){
-    for( i=0; i<rowsCount; i++){
-        newrow = mytable.insertRow(i);
-            for( j=0; j<columsCount+1; j++){
-                 newCell = newrow.insertCell(j);
-            }
-    }
-    newrow = mytable.insertRow(i);
-        for( j=0; j<columsCount; j++){
-             newCell = newrow.insertCell(j);
-        }
-}
-function fillTableWithData(){
-    for(var i=0;i<rowsCount;i++){
-        for(var j=0;j<columsCount;j++){
-            mytable.rows[i].cells[j].innerHTML=arrayOfObjects[i][j].amount;
-            mytable.rows[i].cells[j].id = arrayOfObjects[i][j].id;
-        }
-    }
-}
-
-function fillAverageBlockWithData(){
-    for (var i=0; i<columsCount; i++){
-        var sum=0;
-        for(var j=0; j<rowsCount; j++){
-           sum += arrayOfObjects[j][i].amount;
-        }
-        mytable.rows[rowsCount].cells[i].innerHTML = (parseInt(sum/columsCount*10)/10);
-    }
-    
-}
-
-function fillSumBlockWithData(){
-    for (var i=0; i<rowsCount; i++){
-        var sum=0;
-        for(var j=0; j<columsCount; j++){
-           sum += arrayOfObjects[i][j].amount;
-        }
-        mytable.rows[i].cells[columsCount].innerHTML = sum;
-    }
-}
-
-function createClassForFindPercent() {
-    for (var i=0; i<rowsCount; i++){
-        for(var j=0; j<=columsCount; j++){
-            mytable.rows[i].cells[j].className=i;
-        }
+    for(var j=0; j<columsCount; j++){
+        arrayOfObjectsClone.push(arrayOfObjects[rowsCount-1][j]);
     }
 }
 
 function onCellClick(event) {
     var id = event.target.id;
     increaseDataObjectAmount(id);
-    fillTableWithData();
-    fillAverageBlockWithData();
-    fillSumBlockWithData();
+    startModule.fillTableWithData();
+    startModule.fillAverageBlockWithData();
+    startModule.fillSumBlockWithData();
 }
 
 function increaseDataObjectAmount(id){    
@@ -166,32 +175,21 @@ function getDataObjectById(id){
 }
 
 
-function addEventLisener(){
-    for( i=0; i<rowsCount; i++){       
-            for( j=0; j<columsCount; j++){
-                mytable.rows[i].cells[j].onclick = onCellClick;
-                mytable.rows[i].cells[j].onmouseover = onMouseOverCell;
-                mytable.rows[i].cells[j].onmouseout =  onMouseOutCell;
-                mytable.rows[i].cells[columsCount].onmouseover = onMouseOverSumBlock;
-                mytable.rows[i].cells[columsCount].onmouseout =  onMouseOutSumBlock;
 
-            }
-    }
-}
 
 
 function onMouseOverCell(event){
     var obj = getDataObjectById(event.target.id);
     deleteAmountFromArray(obj.id);
     findNearestElementInArray(obj.amount);
-    createCloneArray();
+    startModule.createCloneArray();
     deleteAmountFromArray(obj.id);
     createTableIdArrayForIdllumination();
     illuminationTable();
 }
 
 function onMouseOutCell(event){
-    createCloneArray();
+    startModule.createCloneArray();
     for (i=0; i<arrayOfIdIlluminationOfElements.length; i++){
         document.getElementById(arrayOfIdIlluminationOfElements[i]).style.background= 'linear-gradient(to right, #AFCDE7 100%, #AFCDE7 0%)';
     }
@@ -281,8 +279,8 @@ function  buttonClickHandlerForDeleteRow() {
     preparationDeleteAmountFromArray();
     deleteRow();
     rowsCount--;
-    fillAverageBlockWithData();
-    fillSumBlockWithData();
+    startModule.fillAverageBlockWithData();
+    startModule.fillSumBlockWithData();
 
 
 }
@@ -302,3 +300,9 @@ function preparationDeleteAmountFromArray(){
 }
 
 
+function createDatObject(){
+    var obj = new Object();
+    obj.amount = Math.floor(Math.random()*(1000-100))+100;
+    obj.id=Math.floor(Math.random()*100000);
+    return obj;
+}
